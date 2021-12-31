d8b0f5dceec8a5fa26062d2e3bd44c0c3f7d2809
Mateusz Malinowski <malina314@op.pl>
2021-12-31 15:27:15 +0100

    dodanie mutexa w korzeniu

--- Tree.c
+++ Tree.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <pthread.h>
 
 // Błąd oznaczający, że `target` jest w poddrzewie `source`.
 #define EINVALIDTARGET -1
@@ -19,58 +20,82 @@
         }                   \
     } while (0)
 
-
-struct Tree {
+typedef struct  {
     HashMap* subfolders;
     ReadWriteLock lock;
+} TreeNode;
+
+struct Tree {
+    TreeNode* root;
+    // `tree_move` ingeruje w dwa węzły, więc musi założyć dwa zamki. Aby uniknąć
+    // zakleszczenia, używam mutexa, w celu założenia obu zamków atomowo.
+    pthread_mutex_t move_mutex;
 };
 
-Tree* tree_new() {
-    Tree* node = malloc(sizeof (Tree));
+static inline TreeNode* tree_new_node() {
+    TreeNode* node = malloc(sizeof (Tree));
     CHECK_PTR(node);
     node->subfolders = hmap_new();
     CHECK_PTR(node->subfolders);
     return node;
 }
 
-void tree_free(Tree* tree) {
+Tree* tree_new() {
+    Tree* tree = malloc(sizeof (Tree));
+    CHECK_PTR(tree);
+    if (pthread_mutex_init(&tree->move_mutex, 0) != 0) {
+        free(tree);
+        return NULL;
+    }
+    tree->root = tree_new_node();
+    return tree;
+}
+
+static inline void tree_free_node(TreeNode* tree_node) {
     const char* key;
     void* subtree;
-    HashMapIterator it = hmap_iterator(tree->subfolders);
-    while (hmap_next(tree->subfolders, &it, &key, &subtree)) {
-     tree_free(subtree);
+    HashMapIterator it = hmap_iterator(tree_node->subfolders);
+    while (hmap_next(tree_node->subfolders, &it, &key, &subtree)) {
+     tree_free_node(subtree);
     }
-    free(tree->subfolders);
-    free(tree);
+    free(tree_node->subfolders);
+    free(tree_node);
+}
+
+void tree_free(Tree* tree) {
+    tree_free_node(tree->root);
+    pthread_mutex_destroy(&tree->move_mutex);
 }
 
 // Zwraca wskaźnik na znaleziony węzeł, jeżeli istnieje, wpp. zwraca NULL.
-static Tree* tree_find(Tree* tree, const char* path) {
-    if (path == NULL || tree == NULL) {
+static TreeNode* tree_find(TreeNode* tree_node, const char* path) {
+    if (path == NULL || tree_node == NULL) {
         return NULL;
     }
     if (strcmp(path, "/") == 0) {
-        return tree;
+        return tree_node;
     }
     char* component = malloc(MAX_FOLDER_NAME_LENGTH + 1);
     CHECK_PTR(component);
     path = split_path(path, component);
-    Tree* subtree = hmap_get(tree->subfolders, component);
+    TreeNode* subtree = hmap_get(tree_node->subfolders, component);
     free(component);
     return tree_find(subtree, path);
 }
 
 char* tree_list(Tree* tree, const char* path) {
+    TreeNode* tree_node = tree->root;
     if (is_path_valid(path)) {
-        tree = tree_find(tree, path);
-        if (tree) {
-            return make_map_contents_string(tree->subfolders);
+        tree_node = tree_find(tree_node, path);
+        if (tree_node) {
+            return make_map_contents_string(tree_node->subfolders);
         }
     }
     return NULL;
 }
 
 int tree_create(Tree* tree, const char* path) {
+    TreeNode* tree_node = tree->root;
     if (strcmp(path, "/") == 0) {
         return EEXIST;
     }
@@ -78,11 +103,11 @@ int tree_create(Tree* tree, const char* path) {
         char* new_subfolder_name = malloc(MAX_FOLDER_NAME_LENGTH + 1);
         CHECK_PTR(new_subfolder_name);
         char* parent_path = make_path_to_parent(path, new_subfolder_name);
-        tree = tree_find(tree, parent_path);
-        if (tree) {
-            if (hmap_get(tree->subfolders, new_subfolder_name) == NULL) {
-                Tree* new_subfolder = tree_new();
-                hmap_insert(tree->subfolders, new_subfolder_name, new_subfolder);
+        tree_node = tree_find(tree_node, parent_path);
+        if (tree_node) {
+            if (hmap_get(tree_node->subfolders, new_subfolder_name) == NULL) {
+                TreeNode* new_subfolder = tree_new_node();
+                hmap_insert(tree_node->subfolders, new_subfolder_name, new_subfolder);
                 return 0;
             }
             return EEXIST;
@@ -93,6 +118,7 @@ int tree_create(Tree* tree, const char* path) {
 }
 
 int tree_remove(Tree* tree, const char* path) {
+    TreeNode* tree_node = tree->root;
     if (strcmp(path, "/") == 0) {
         return EBUSY;
     }
@@ -100,14 +126,14 @@ int tree_remove(Tree* tree, const char* path) {
         char* subfolder_to_remove = malloc(MAX_FOLDER_NAME_LENGTH + 1);
         CHECK_PTR(subfolder_to_remove);
         char* parent_path = make_path_to_parent(path, subfolder_to_remove);
-        tree = tree_find(tree, parent_path);
-        if (tree) {
-            Tree* to_remove = hmap_get(tree->subfolders, subfolder_to_remove);
+        tree_node = tree_find(tree_node, parent_path);
+        if (tree_node) {
+            TreeNode* to_remove = hmap_get(tree_node->subfolders, subfolder_to_remove);
             if (to_remove) {
                 if (hmap_size(to_remove->subfolders) == 0) {
                     hmap_free(to_remove->subfolders);
                     free(to_remove);
-                    hmap_remove(tree->subfolders, subfolder_to_remove);
+                    hmap_remove(tree_node->subfolders, subfolder_to_remove);
                     return 0;
                 }
                 return ENOTEMPTY;
@@ -119,11 +145,12 @@ int tree_remove(Tree* tree, const char* path) {
 }
 
 // Sprawdza, czy `pre` jest prefiksem `str`.
-static inline bool is_prefix(const char *pre, const char *str) {
+static inline bool is_prefix(const char* pre, const char* str) {
     return strncmp(pre, str, strlen(pre)) == 0;
 }
 
 int tree_move(Tree* tree, const char* source, const char* target) {
+    TreeNode* tree_node = tree->root;
     if (strcmp(source, "/") == 0) {
         return EBUSY;
     }
@@ -134,14 +161,14 @@ int tree_move(Tree* tree, const char* source, const char* target) {
         char* source_name = malloc(MAX_FOLDER_NAME_LENGTH + 1);
         CHECK_PTR(source_name);
         char* source_parent_path = make_path_to_parent(source, source_name);
-        Tree* source_parent_tree = tree_find(tree, source_parent_path);
+        TreeNode* source_parent_tree = tree_find(tree_node, source_parent_path);
         if (source_parent_tree) {
-            char *target_name = malloc(MAX_FOLDER_NAME_LENGTH + 1);
+            char* target_name = malloc(MAX_FOLDER_NAME_LENGTH + 1);
             CHECK_PTR(target_name);
-            char *target_parent_path = make_path_to_parent(target, target_name);
-            Tree *target_parent_tree = tree_find(tree, target_parent_path);
+            char* target_parent_path = make_path_to_parent(target, target_name);
+            TreeNode* target_parent_tree = tree_find(tree_node, target_parent_path);
             if (target_parent_tree) {
-                Tree *to_move = hmap_get(source_parent_tree->subfolders, source_name);
+                TreeNode* to_move = hmap_get(source_parent_tree->subfolders, source_name);
                 if (to_move) {
                     if (strcmp(source, target) == 0) {
                         return 0;
