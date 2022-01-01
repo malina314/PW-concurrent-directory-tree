5b3cc63ee00a730f40437458cf4c3dd16a598f61
Mateusz Malinowski <malina314@op.pl>
2022-01-01 16:14:55 +0100

    add locks (and deadlocks)

--- Tree.c
+++ Tree.c
@@ -1,3 +1,5 @@
+// todo: poczytać doc o mutexach i zmienić to 0 na NULL
+
 // todo: komentarz
 // autor: Mateusz Malinowski (mm429561)
 // W zadaniu występuje problem analogiczny jak "czytelnicy i pisarze". Rolę
@@ -42,16 +44,19 @@ struct Tree {
 
 // Tworzy nowy węzeł drzewa.
 static inline TreeNode* tree_new_node() {
-    TreeNode* node = malloc(sizeof (Tree));
+    TreeNode* node = malloc(sizeof (TreeNode));
     CHECK_PTR(node);
     node->subfolders = hmap_new();
     CHECK_PTR(node->subfolders);
+    //todo: obsługa błędu
+    rwlock_init(&node->lock);
     return node;
 }
 
 Tree* tree_new() {
     Tree* tree = malloc(sizeof (Tree));
     CHECK_PTR(tree);
+    //todo: może lepsza obsługa błędu?
     if (pthread_mutex_init(&tree->move_mutex, 0) != 0) {
         free(tree);
         return NULL;
@@ -69,11 +74,14 @@ static inline void tree_free_node(TreeNode* tree_node) {
      tree_free_node(subtree);
     }
     free(tree_node->subfolders);
+    //todo: obsługa błędu
+    rwlock_destroy(&tree_node->lock);
     free(tree_node);
 }
 
 void tree_free(Tree* tree) {
     tree_free_node(tree->root);
+    //todo: obsługa błędu
     pthread_mutex_destroy(&tree->move_mutex);
 }
 
@@ -83,12 +91,17 @@ static TreeNode* tree_find(TreeNode* tree_node, const char* path) {
         return NULL;
     }
     if (strcmp(path, "/") == 0) {
+        // Zakładamy zamek, który zostanie zwolniony przez funkcję, która
+        // wywołała `tree_find`.
+        rwlock_before_read(&tree_node->lock);
         return tree_node;
     }
     char* component = malloc(MAX_FOLDER_NAME_LENGTH + 1);
     CHECK_PTR(component);
     path = split_path(path, component);
+    rwlock_before_read(&tree_node->lock);
     TreeNode* subtree = hmap_get(tree_node->subfolders, component);
+    rwlock_after_read(&tree_node->lock);
     free(component);
     return tree_find(subtree, path);
 }
@@ -98,7 +111,9 @@ char* tree_list(Tree* tree, const char* path) {
     if (is_path_valid(path)) {
         tree_node = tree_find(tree_node, path);
         if (tree_node) {
-            return make_map_contents_string(tree_node->subfolders);
+            char* list = make_map_contents_string(tree_node->subfolders);
+            rwlock_after_read(&tree_node->lock); // Zwalniamy zamek z `tree_find`.
+            return list;
         }
     }
     return NULL;
@@ -115,9 +130,12 @@ int tree_create(Tree* tree, const char* path) {
         char* parent_path = make_path_to_parent(path, new_subfolder_name);
         tree_node = tree_find(tree_node, parent_path);
         if (tree_node) {
+            rwlock_after_read(&tree_node->lock); // Zwalniamy zamek z `tree_find`.
+            rwlock_before_write(&tree_node->lock);
             if (hmap_get(tree_node->subfolders, new_subfolder_name) == NULL) {
                 TreeNode* new_subfolder = tree_new_node();
                 hmap_insert(tree_node->subfolders, new_subfolder_name, new_subfolder);
+                rwlock_after_write(&tree_node->lock);
                 return 0;
             }
             return EEXIST;
@@ -139,15 +157,20 @@ int tree_remove(Tree* tree, const char* path) {
         tree_node = tree_find(tree_node, parent_path);
         if (tree_node) {
             TreeNode* to_remove = hmap_get(tree_node->subfolders, subfolder_to_remove);
+            rwlock_after_read(&tree_node->lock); // Zwalniamy zamek z `tree_find`.
+            rwlock_before_write(&tree_node->lock);
             if (to_remove) {
                 if (hmap_size(to_remove->subfolders) == 0) {
                     hmap_free(to_remove->subfolders);
                     free(to_remove);
                     hmap_remove(tree_node->subfolders, subfolder_to_remove);
+                    rwlock_after_write(&tree_node->lock);
                     return 0;
                 }
+                rwlock_after_write(&tree_node->lock);
                 return ENOTEMPTY;
             }
+            rwlock_after_write(&tree_node->lock);
         }
         return ENOENT;
     }
@@ -179,20 +202,33 @@ int tree_move(Tree* tree, const char* source, const char* target) {
             TreeNode* target_parent_tree = tree_find(tree_node, target_parent_path);
             if (target_parent_tree) {
                 TreeNode* to_move = hmap_get(source_parent_tree->subfolders, source_name);
+                //todo: obsługa błędów
+                pthread_mutex_lock(&tree->move_mutex);
+                    // todo: jeżeli target == source to się chyba zakleszczy
+                rwlock_before_write(&source_parent_tree->lock);
+                rwlock_before_write(&target_parent_tree->lock);
+                rwlock_after_read(&source_parent_tree->lock); // Zwalniamy zamek z `tree_find`.
+                rwlock_after_read(&target_parent_tree->lock); // Zwalniamy zamek z `tree_find`.
+                int return_value = 0;
                 if (to_move) {
                     if (strcmp(source, target) == 0) {
-                        return 0;
+                        return_value = 0;
+                    } else if (is_prefix(source, target)) {
+                        return_value = EINVALIDTARGET;
+                    } else if (hmap_get(target_parent_tree->subfolders, target_name) != NULL) {
+                        return_value = EEXIST;
+                    } else {
+                        hmap_remove(source_parent_tree->subfolders, source_name);
+                        hmap_insert(target_parent_tree->subfolders, target_name, to_move);
                     }
-                    if (is_prefix(source, target)) {
-                        return EINVALIDTARGET;
-                    }
-                    if (hmap_get(target_parent_tree->subfolders, target_name) != NULL) {
-                        return EEXIST;
-                    }
-                    hmap_remove(source_parent_tree->subfolders, source_name);
-                    hmap_insert(target_parent_tree->subfolders, target_name, to_move);
-                    return 0;
+                } else {
+                    return_value = ENOENT;
                 }
+                //todo: obsługa błędów
+                rwlock_after_write(&target_parent_tree->lock);
+                rwlock_after_write(&source_parent_tree->lock);
+                pthread_mutex_unlock(&tree->move_mutex);
+                return return_value;
             }
         }
         return ENOENT;
